2dd9465f2830eb01ac89b921f1d188f408f94fde
MrRush <98553195+MrRush76@users.noreply.github.com>
2024-09-11 13:50:20 +0100

    Update Sequence.py

--- Sequence.py
+++ Sequence.py
@@ -1,5 +1,5 @@
 numbersGenerated = input("How many numbers of the Fibonacci would you like to generate?\n") # decides how many numbers of the fibonacci sequence will be generated
-sequence = [0, 1] # the first twi terms of the fibonacci sequence
+sequence = [0, 1] # the first two terms of the fibonacci sequence
 for i in range(int(numbersGenerated) -2): # starts a loop to generate how many numbers have been inputted
     nextTerm = sequence[i] + sequence[i+1] # generates the next term by adding the previous two terms
     sequence.append(nextTerm)  # adds the next term to the list
